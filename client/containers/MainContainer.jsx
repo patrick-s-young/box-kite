f0908e564799820b410d7eb47abc26056cefdddf
Patrick Young <patrickyoung@Patricks-MacBook-Pro.local>
2019-11-15 11:08:07 -0800

    Introduced react-spring

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { cuboidData } from './MainContainerData.jsx';
 import Cuboid from '../components/Cuboid.jsx';
+import { useSpring, animated, interpolate } from 'react-spring';
+
 
 export default () => {
 
   const [ cuboidIndex, setCuboidIndex ] = useState(0);
+
   const [ isExploded, setIsExploded ] = useState(false);
+
+  const [ boxSpring, setBoxSpring ] = useSpring(() => ({scale: 0.3}));
+
+  const [ boxSpringToggle, setBoxSpringToggle ] = useState(false);
 
   const clickButtonHandler = (action) => {
     switch(action) {
@@ -15,6 +22,11 @@ export default () => {
       case 'previous':
         setCuboidIndex(cuboidIndex > 0 ? cuboidIndex - 1 : cuboidData.cuboids.length - 1);
         break;
+      case 'scale':
+        // 
+        console.log(`in clickButtonHandler with action = ${action}`);
+        setBoxSpringToggle(!boxSpringToggle);
+        break;
       case 'explode':
         setIsExploded(!isExploded);
         break;
@@ -23,16 +35,29 @@ export default () => {
     }
   }
 
+  useEffect(() => {
+    if (boxSpringToggle === false) {
+      setBoxSpring({from: {scale: 1.0}, to: {scale: 0.3}});
+    } else {
+      setBoxSpring({from: {scale: 0.3}, to: {scale: 1.0}});
+    }
+  }, [boxSpringToggle]);
 
  
 
   return (
     <div>
 
+    <animated.div 
+      onClick={     () => clickButtonHandler('scale')}
+      style={{      transform: boxSpring.scale.interpolate(scale => `scale3d(${scale},${scale},${scale})`)
+
+                    }}>
+
     <Cuboid 
       size={        cuboidData.cuboids[cuboidIndex].size} 
       imagePath={   cuboidData.cuboids[cuboidIndex].imagePath} 
-      isExploded={  isExploded}/>
+      isExploded={  isExploded}/></animated.div>
 
     <button 
       onClick={     () => clickButtonHandler('previous')}
@@ -53,8 +78,6 @@ export default () => {
       style={{      height: "100px", width: "100px", opacity: 0.5}}>
                     {`EXPLODE`}
                     </button>
-
-
     </div>
   );
 }
